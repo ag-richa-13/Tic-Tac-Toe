3fddc79d6e9182e8ff2d0d2c026c557c072ef87c
Richa Agrawal <agrawalricha013@gmail.com>
2024-11-29 18:58:45 +0530

    Add Splash Scene

---
+++ Assets/Script/SplashManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public float splashDuration = 3f; // Duration of the splash scene
+    void Start()
+    {
+        // Automatically load the next scene after the specified duration
+        Invoke("LoadNextScene", splashDuration);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene("GameScene"); // Replace with your scene's name
+    }
+}
